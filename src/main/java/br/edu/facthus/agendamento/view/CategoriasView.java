7d332a0837b2cd75ba3be83427bce10c9de1754f
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-14 22:14:57 -0300

    Update RQ 1.1.2.2: Acrescenta o bean categorias.

--- src/main/java/br/edu/facthus/agendamento/view/CategoriasView.java
+++ src/main/java/br/edu/facthus/agendamento/view/CategoriasView.java
@@ -1,13 +1,14 @@
 package br.edu.facthus.agendamento.view;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.edu.facthus.agendamento.bean.CategoriasBean;
 import br.edu.facthus.agendamento.entity.Categoria;
 
 @Named
@@ -16,30 +17,14 @@ public class CategoriasView implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	private CategoriasBean categoriasBean;
+	
 	private List<Categoria> categorias;
 	
 	@PostConstruct
 	public void init() {
-		// TODO: categorias criadas para teste
-		Categoria c1 = new Categoria();
-		c1.setId(1);
-		c1.setDescricao("Informática");
-		c1.setAtiva(true);
-		
-		Categoria c2 = new Categoria();
-		c2.setId(2);
-		c2.setDescricao("Audiovisual");
-		c2.setAtiva(true);
-		
-		Categoria c3 = new Categoria();
-		c3.setId(3);
-		c3.setDescricao("Materiais Elétricos");
-		c3.setAtiva(true);
-		
-		categorias = new ArrayList<>();
-		categorias.add(c1);
-		categorias.add(c2);
-		categorias.add(c3);
+		categorias = categoriasBean.buscaCategorias();
 	}
 
 	public List<Categoria> getCategorias() {
