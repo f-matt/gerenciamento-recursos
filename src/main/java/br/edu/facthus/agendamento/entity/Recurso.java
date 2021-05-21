5ed2da17e77be83f9b79cfa3da8c0685dbcc2654
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-05-21 19:50:15 -0300

    Fix RQ 2.2.1.2: inserção de recursos

--- src/main/java/br/edu/facthus/agendamento/entity/Recurso.java
+++ src/main/java/br/edu/facthus/agendamento/entity/Recurso.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +22,40 @@ public class Recurso implements Serializable {
 	private String descricao;
 	
 	private Boolean ativa;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
+	@Override
+	public String toString() {
+		return "Recurso [id=" + id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recurso other = (Recurso) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 	public Integer getId() {
 		return id;
@@ -45,5 +81,12 @@ public class Recurso implements Serializable {
 		this.ativa = ativa;
 	}
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
