cd9c849b4b62631a40ddf6415634e7b956075809
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-06-02 21:52:49 -0300

    Validação RQ9, RQ12, RQ13 e RQ15

--- src/main/java/br/edu/facthus/agendamento/entity/Recurso.java
+++ src/main/java/br/edu/facthus/agendamento/entity/Recurso.java
@@ -8,10 +8,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "recursos")
+@NamedQueries({
+	@NamedQuery(name = "Recurso.findByDescricao",
+			query = "SELECT r "
+					+ "FROM Recurso r "
+					+ "WHERE UPPER(r.descricao) LIKE :descricao "
+					+ "ORDER BY r.descricao")
+})
 public class Recurso implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
