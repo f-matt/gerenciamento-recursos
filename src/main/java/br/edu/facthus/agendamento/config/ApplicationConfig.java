e1104f57e67a21572bbc2e1ac8843a159e2f952a
Fernando Mattioli <fernando.mattioli@facthus.edu.br>
2021-10-01 19:40:07 -0300

    Fix T27: configura a autenticação HTTP

---
+++ src/main/java/br/edu/facthus/agendamento/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package br.edu.facthus.agendamento.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
+
+@BasicAuthenticationMechanismDefinition
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
